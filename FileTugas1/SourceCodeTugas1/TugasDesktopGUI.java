7aef9e1fcced42ea53cf81206fe6fb9c15c98318
Ahmad Rifai <87814697+Ahmdrf27@users.noreply.github.com>
2022-03-15 15:34:50 +0700

    Add files via upload

---
+++ FileTugas1/SourceCodeTugas1/TugasDesktopGUI.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tugasdesktopgui;
+
+/**
+ *
+ * @author HP
+ */
+public class TugasDesktopGUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Input inp = new Input();
+        inp.setVisible(true);
+        inp.pack();
+        inp.setLocationRelativeTo(null);
+        inp.setDefaultCloseOperation(Input.EXIT_ON_CLOSE);
+    }
+}
